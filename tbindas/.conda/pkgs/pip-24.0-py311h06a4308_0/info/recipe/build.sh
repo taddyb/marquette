02b7335d1a71a00431445ba9c08e115112124c41
Tadd Bindas <tkb5476@psu.edu>
2024-06-16 12:12:47 -0500

    finished q_prime sum algorithm

--- tbindas/.conda/pkgs/pip-24.0-py311h06a4308_0/info/recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install --single-version-externally-managed --record record.txt
-
-cd $PREFIX/bin
-rm -f pip2* pip3*
-rm -f $SP_DIR/__pycache__/pkg_res*
-# Remove all bundled .exe files courtesy of distlib.
-rm -f $SP_DIR/pip/_vendor/distlib/*.exe
